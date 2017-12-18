84c6cfebcbb5131f2af3b1e68a9feea3853ff32d
Sarah Yu <sarahyu0692@gmail.com>
2017-12-18 14:18:53 -0500

    finish coffee show view

--- src/components/FlavorProfile/FlavorProfile.js
+++ src/components/FlavorProfile/FlavorProfile.js
@@ -1,48 +1,24 @@
 import React, { Component } from 'react'
-import axios from 'axios'
 
 class FlavorProfile extends Component {
-	constructor() {
-		super()
-		this.state = {
-			body: null,
-			acidity: null,
-			notes: [],
-			id: null
-		}
-	}
-	componentDidMount() {
-		axios
-			.get(`#`)
-			.then(res => {
-				this.setState({
-					body: res.data.body,
-					acidity: res.data.acidity,
-					notes: res.data.notes,
-					id: res.data._id
-				})
-			})
-			.catch(err => {
-				console.log(err)
-			})
-	}
-
 	render() {
 		return (
 			<div>
-				<div className="flavor-profile-body">
-					<div className="flavor-profile-tag"> {this.props.body} body </div>
-				</div>
-				<div className="flavor-profile-acidity">
-					<div className="flavor-profile-tag">{this.state.acidity} acidity</div>
-				</div>
-				<div className="flavor-profile-notes">
-					{this.state.notes.isFruity ? (
-						<div className="flavor-profile-tag"> Fruity </div>
-					) : (
-						<div />
-					)}
-				</div>
+				<h3>Flavor Profile:</h3>
+				<h4>Body: {this.props.body}</h4>
+				<h4>Acidity: {this.props.acidity}</h4>
+				<h4>Notes:</h4>
+				<ul>
+					{this.props.isWeird ? <li>Weird</li> : ''}
+					{this.props.isFloral ? <li>Floral</li> : ''}
+					{this.props.isSweet ? <li>Sweet</li> : ''}
+					{this.props.isNuttyCocoa ? <li>Nutty / Chocolatey</li> : ''}
+					{this.props.isSpicy ? <li>Spicy</li> : ''}
+					{this.props.isRoasted ? <li>Roasted</li> : ''}
+					{this.props.isVeggie ? <li>Earthy / Green</li> : ''}
+					{this.props.isSour ? <li>Sour / Fermented</li> : ''}
+					{this.props.isFruity ? <li>Fruity</li> : ''}
+				</ul>
 			</div>
 		)
 	}
