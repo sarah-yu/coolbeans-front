eab526f98731226875a356f517c2d96301514f1a
Declan Kurant <Kurant_Declan@bah.com>
2017-12-18 17:01:10 -0500

    reorganized welcome view

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -4,23 +4,40 @@ import { Link } from 'react-router-dom'
 import './Welcome.css'
 
 const Welcome = () => (
-	<section className="welcome">
-		<h1>Welcome to Cool Beans! Click a link to get started:</h1>
-		<nav className="welcome-nav">
-			<ul>
-				<li>
-					<Link to="/resources">Resources</Link> - Learn more about coffee
-					terminology and how we categorize our coffees
-				</li>
-				<li>
-					<Link to="/coffees">View Coffees</Link> - See all the coffees
-				</li>
-				<li>
-					<Link to="/search">Search Coffees</Link> - Get coffee recommendations
-				</li>
-			</ul>
-		</nav>
-	</section>
+	<div className="welcome">
+		<div className="welcome-row">
+			<h3> Find a coffee to match your tastes... </h3>
+			<Link to="/search">
+				<button>Recommend Me Coffees</button>
+			</Link>
+			<img
+				id="welcome-img"
+				src="https://i.pinimg.com/736x/93/a8/64/93a8642126dbc3e87899e2154bc1ba03--tumblr-tea-coffee-tumblr.jpg"
+			/>
+		</div>
+
+		<div className="welcome-row">
+			<h3> Learn more about different types of coffee... </h3>
+			<Link to="/resources">
+				<button>View Learning Resources</button>
+			</Link>
+			<img
+				id="welcome-img"
+				src="http://gdruk.com/wp-content/uploads/2016/08/coffee-shop.jpg"
+			/>
+		</div>
+
+		<div className="welcome-row">
+			<h3> See our full coffee bean catalogue </h3>
+			<Link to="/resources">
+				<button>Show All Coffees</button>
+			</Link>
+			<img
+				id="welcome-img"
+				src="http://archive.c4eo.org.uk/themes/vulnerablechildren/eresource/images/vc/frontline/main/bonus4.jpg"
+			/>
+		</div>
+	</div>
 )
 
 export default Welcome
