8902afaccb8760f199d0b9d05179799c39895b73
Sarah Yu <sarahyu0692@gmail.com>
2017-12-18 16:06:24 -0500

    add css

--- src/components/CoffeeList/CoffeeList.js
+++ src/components/CoffeeList/CoffeeList.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 
+import './CoffeeList.css'
+
 class CoffeeList extends Component {
 	constructor(props) {
 		super(props)
@@ -29,7 +31,25 @@ class CoffeeList extends Component {
 			return (
 				<div key={index} className="coffee">
 					<Link to={`/coffees/${coffee._id}`}>
-						<h2>{coffee.name}</h2>
+						<div className="coffee-img">
+							<img src={coffee.imgUrl} alt={coffee.name} />
+						</div>
+						<h2>
+							{coffee.name}{' '}
+							<span class="coffee-notes">
+								{coffee.flavorProfile[0].notes.isFruity ? 'Fruity ' : ''}
+								{coffee.flavorProfile[0].notes.isSour ? 'Sour ' : ''}
+								{coffee.flavorProfile[0].notes.isVeggie ? 'Earthy ' : ''}
+								{coffee.flavorProfile[0].notes.isRoasted ? 'Roasted ' : ''}
+								{coffee.flavorProfile[0].notes.isSpicy ? 'Spicy ' : ''}
+								{coffee.flavorProfile[0].notes.isNuttyCocoa
+									? 'Nutty / Chocolatey '
+									: ''}
+								{coffee.flavorProfile[0].notes.isSweet ? 'Sweet ' : ''}
+								{coffee.flavorProfile[0].notes.isFloral ? 'Floral ' : ''}
+								{coffee.flavorProfile[0].notes.isWeird ? 'Weird ' : ''}
+							</span>
+						</h2>
 					</Link>
 				</div>
 			)
