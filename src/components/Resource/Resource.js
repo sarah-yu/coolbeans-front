72987515fba5bbb7a151386dde3eb3a126ee44b8
Sarah Yu <sarahyu0692@gmail.com>
2017-12-18 16:56:42 -0500

    format resources page

--- src/components/Resource/Resource.js
+++ src/components/Resource/Resource.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react'
 
+import './Resource.css'
+
 class Resource extends Component {
 	render() {
 		return (
-			<div className="resources">
-				<div className="resourcesWelcome">
-					<h1>
+			<section className="resources">
+				<div className="resources-welcome">
+					<h2>
 						Welcome to the Resources page! Here you can find a glossary of terms
-						present throughout our site!
-					</h1>
+						present throughout our site.
+					</h2>
 				</div>
 
-				<div className="jargon">
-					<h4>
+				<div className="resources-jargon">
+					<h3>
 						There are a lot of terms to describe coffee that may seem confusing!
-						Here's a condensed list of terms we use throughout our site.
-					</h4>
+						Here is a condensed list of terms we use throughout our site.
+					</h3>
 					<ul>
 						<li>
 							Acidity: Acidity refers to the liveliness or sharpness the coffee
@@ -30,7 +32,7 @@ class Resource extends Component {
 						</li>
 						<li>
 							Flavor: Unsurprisingly, flavor refers to the overall taste of the
-							drink. It's the combination of the acidity & body along with the
+							drink. It is the combination of the acidity & body along with the
 							notes present in the coffee.
 						</li>
 						<li>
@@ -42,39 +44,38 @@ class Resource extends Component {
 				</div>
 
 				<div className="notes">
-					<h4>
+					<h3>
 						Below you can find a list of flavor notes we focused on in our
 						registry!
-					</h4>
+					</h3>
 					<ul>
 						<li>
-							Fruity: 'Fruity' notes refer to fruit-like qualities in the
-							coffee. It can vary from a coffee having strawberry undertones to
-							canteloupe understones!
+							Fruity: Refers to fruit-like qualities in the coffee. It can vary
+							from a coffee having strawberry undertones to canteloupe
+							understones!
 						</li>
 						<li>
-							Sour: 'Sour' refers to the tartness of the coffee. Many of these
-							notes tend to be fruity in nature; think about the 'sourness' of
-							citrus fruits or granny smith apples.
+							Sour: Refers to the tartness of the coffee. Many of these notes
+							tend to be fruity in nature; think about the 'sourness' of citrus
+							fruits or granny smith apples.
 						</li>
 						<li>
-							Veggie: 'Veggie' refers to 'green' notes, notes that are derived
-							from plants or earthy flavors. For example, earthy tones such as
-							olive oil or peapods would be considered veggie.
+							Earthy: Refers to 'green' notes, notes that are derived from
+							plants. For example, vegetal tones such as olive oil or peapods
+							would be considered earthy.
 						</li>
 						<li>
-							Roasted: 'Roasted' refers to flavors reminiscent of roasted
-							things, like cereal. Grainy, malt-y, even tobacco undertones in
-							the coffee are categorized as 'roasted'.
+							Roasted: Refers to flavors reminiscent of roasted things, like
+							cereal. Grainy, malt-y, even tobacco undertones in the coffee are
+							categorized as 'roasted'.
 						</li>
 						<li>
-							Spicy: Spicy refers to flavors remiscent of different spices.
-							Cinnamon, nutmeg, anise understones would all be considered
-							"spicy".
+							Spicy: Refers to flavors remiscent of different spices. Cinnamon,
+							nutmeg, anise understones would all be considered "spicy".
 						</li>
 						<li>
-							Nutty/Cocoa: Nutty/Cocoa indicates the presence of either nutty
-							(like peanut or almond) undertones or chocolate-y (dark or light)
+							Nutty / Chocolatey: Indicates the presence of either nutty (like
+							peanut or almond) undertones or chocolatey (dark or light)
 							undertones in the coffee.
 						</li>
 						<li>
@@ -87,29 +88,29 @@ class Resource extends Component {
 							reminiscent of teas, such as lavender, jasmine, & coriander.
 						</li>
 						<li>
-							Weird: Weird, or exotic, notes is a catch-all for flavors that
-							don't fit under the other categories. This category runs the gamut
+							Weird: Weird, or exotic, notes is a catch-all for flavors that do
+							not fit under the other categories. This category runs the gamut
 							from woody to rubbery!
 						</li>
 					</ul>
 				</div>
 
 				<div className="images">
-					<h4> Here are some images that might be useful to you!</h4>
+					<h3> Here are some images that might be useful to you!</h3>
 
-					<h5>Flavor Wheel:</h5>
+					<h4>Flavor Wheel:</h4>
 					<img
 						src="http://www.scanews.coffee/wp-content/uploads/2016/01/SCAA_FlavorWheel.01.18.15.jpg"
 						alt="SCAA Coffee Flavor Wheel"
 					/>
 
-					<h5>Intro to coffee profiles: </h5>
+					<h4>Intro to coffee profiles: </h4>
 					<img
 						src="https://i.pinimg.com/originals/29/2d/38/292d389baba11880be1bfe992ac6e672.jpg"
 						alt="World coffee map"
 					/>
 				</div>
-			</div>
+			</section>
 		)
 	}
 }
