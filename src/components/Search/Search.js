8e0e9720a46d3790adcbefca5483b93741e07941
Sarah Yu <sarahyu0692@gmail.com>
2017-12-18 21:15:26 -0500

    set up way to filter coffees based on body

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Search extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			body: ''
+		}
+	}
+
+	handleBody(e) {
+		this.setState({
+			body: e.target.innerHTML
+		})
+
+		this.getCoffees()
+	}
+
+	getCoffees() {
+		axios.get('http://localhost:3001/coffees').then(res => {
+			let results = res.data.filter(coffee => {
+				let flavorProfile = coffee.flavorProfile[0]
+				return flavorProfile.body === this.state.body
+			})
+			console.log(results)
+		})
+	}
+
 	render() {
 		return (
-			<div>
-				<p>just testing</p>
+			<div className="search">
+				<h2>Body</h2>
+				<button onClick={e => this.handleBody(e)}>Light</button>
+				<button onClick={e => this.handleBody(e)}>Balanced</button>
+				<button onClick={e => this.handleBody(e)}>Full</button>
 			</div>
 		)
 	}
