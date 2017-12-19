79e6e9a1aaea8705f18ab84d72b1ea4b9509b638
Declan Kurant <Kurant_Declan@bah.com>
2017-12-19 11:37:14 -0500

    style welcome page

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -6,10 +6,12 @@ import './Welcome.css'
 const Welcome = () => (
 	<div className="welcome">
 		<div className="welcome-row">
-			<h3> Find a coffee to match your tastes... </h3>
-			<Link to="/search">
-				<button>Recommend Me Coffees</button>
-			</Link>
+			<div className="welcome-row-writing">
+				<h3> Find a coffee to match your tastes... </h3>
+				<Link to="/search">
+					<button className="btn">Discover</button>
+				</Link>
+			</div>
 			<img
 				id="welcome-img"
 				src="https://i.pinimg.com/736x/93/a8/64/93a8642126dbc3e87899e2154bc1ba03--tumblr-tea-coffee-tumblr.jpg"
@@ -17,10 +19,12 @@ const Welcome = () => (
 		</div>
 
 		<div className="welcome-row">
-			<h3> Learn more about different types of coffee... </h3>
-			<Link to="/resources">
-				<button>View Learning Resources</button>
-			</Link>
+			<div className="welcome-row-writing">
+				<h3> Learn about coffee terms... </h3>
+				<Link to="/resources">
+					<button className="btn">Learn</button>
+				</Link>
+			</div>
 			<img
 				id="welcome-img"
 				src="http://gdruk.com/wp-content/uploads/2016/08/coffee-shop.jpg"
@@ -28,13 +32,15 @@ const Welcome = () => (
 		</div>
 
 		<div className="welcome-row">
-			<h3> See our full coffee bean catalogue </h3>
-			<Link to="/resources">
-				<button>Show All Coffees</button>
-			</Link>
+			<div className="welcome-row-writing">
+				<h3> See our full coffee bean catalogue </h3>
+				<Link to="/resources">
+					<button className="btn">Show All</button>
+				</Link>
+			</div>
 			<img
 				id="welcome-img"
-				src="http://archive.c4eo.org.uk/themes/vulnerablechildren/eresource/images/vc/frontline/main/bonus4.jpg"
+				src="https://d2zfkpu1r6ym98.cloudfront.net/sites/guideposts.org/files/story/senior_women_laughing_over_coffee_0.jpg"
 			/>
 		</div>
 	</div>
